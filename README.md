ddcaf86aa9c1c4551e9639c3f2d151beabd7ea82
sbowennn <sbowennn@hotmail.com>
2024-11-27 15:55:50 +0000

    created files and added some content on quiz.js and README files

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# online-quiz
-An online quiz made of geography multiple-choice questions 
+# Online Quiz
+
+---
+
+## An online quiz made of coding multiple-choice questions 
+
+---
+
+This quiz is made to fulfill the project 3 and its requirements for the Front-End Engineer course.
+The quiz tests your knowledge in general and basic coding terms and concepts, spefically related to HTML and CSS, inspired in questions and answers from the course.
+
+---
+
+## How does it work?
+
+- Access the quiz;
+- Answer the questions;
+- Get the results and the correct answers;
+- Try it again to get 100%.
+
---
+++ files/quiz.js
@@ -0,0 +1,192 @@
+const questions = [
+  {
+  // question 1 
+    question: "What does HTML stand for?",
+    options: [
+      "Human Text Modeling Language",
+      "Hypertext Markup Language",
+      "Hyper Tool Markup Language",
+      "Home Tool Markup Language"
+    ],
+    answer: 1
+  },
+  {
+  // question 2 
+    question: "Which html tag contains a web page's metadata?",
+    options: [
+      "<head>",
+      "<article>",
+      "<body>",
+      "<html>"
+    ],
+    answer: 0
+  },
+  {
+  // question 3 
+    question: "What is the difference between the elements <ul> and <ol>?",
+    options: [
+      "There is no visual difference.",
+      "<ul> shows the content, <ol> hides the content.",
+      "<ul> outputs a bullet points' list and <ol> outputs a numbered list.",
+      "<ol> outputs a numbered list and <ul> outputs a bullet points' list."
+    ],
+    answer: 2
+  },
+  {
+  // question 4 
+    question: "What is true about the <video> tag?",
+    options: [
+      "The width and height of the video must be specified.",
+      "The <video> element is not a self-closing tag.",
+      "This valid self-closing tag ends with />.",
+      "This tag displays only images with captions."
+    ],
+    answer: 1
+  },
+  {
+  // question 5 
+    question: "",
+    options: [
+
+    ],
+    answer:
+  },
+  {
+  // question 6 
+    question: "What does CSS stand for?",
+    options: [
+      "Cascading Style Sheets",
+      "Custom Styling Sheets",
+      "Creative Style Sheets",
+      "Content Styling Sheets"
+    ],
+    answer: 0
+  },
+  {
+  // question 7 
+    question: "What is the <link> tag used for in HTML files?",
+    options: [
+      "To make sure all links are styled correctly on the page.",
+      "To embed an external HTML file into the current document for additional page content.",
+      "To link a specific stylesheet file to an HTML file so styles get applied on the page.",
+      "To make sure the hyperlink that navigates to another webpage is defined."
+    ],
+    answer: 2
+  },
+  {
+  // question 8 
+    question: "Which is the correct way to link style.css to index.html?",
+    options: [
+      "<link style.css stylesheet>",
+      "<link href='main.css' rel='style.css'",
+      "<link href='stylesheet' rel='style.css'>",
+      "<link href='style.css' rel='stylesheet'>"
+    ],
+    answer: 3
+  },
+  {
+  // question 9 
+    question: "Which of the following is accomplished by separating HTML and CSS files?",
+    options: [
+      "Enhances webpage load time.",
+      "Code is easier to read and maintain.",
+      "CSS selectors are more specific",
+      "Better-looking CSS styles."
+    ],
+    answer: 1
+  },
+  {
+  // question 10 
+    question: "What is the correct syntax to style multiple selectors?",
+    options: [
+      ".nav-menu; p { };",
+      ".nav-menu: p { };",
+      "#nav-menu; p { };",
+      "#nav.menu, p { };"
+    ],
+    answer: 3
+  },
+  {
+  // question 11 
+    question: "What is the correct syntax to select an element inside another element?",
+    options: [
+      ".home-text li { };",
+      ".home-text_li { };",
+      ".home-text[li] { };",
+      ".home-text=li { };"  
+    ],
+    answer: 0
+  },
+  {
+  // question 12 
+    question: "Which option is correct to set the top and bottom margins to 32 pixels and left and right margins to 16 pixels?",
+    options: [
+  
+    ],
+    answer:
+  },
+  {
+  // question 13 
+    question: "",
+    options: [
+  
+    ],
+    answer:
+  },
+  {
+  // question 14 
+    question: "",
+    options: [
+    
+    ],
+    answer:
+  },
+  {
+  // question 15 
+    question: "",
+    options: [
+  
+    ],
+    answer:
+  },
+  {
+  // question 16 
+    question: "",
+    options: [
+  
+    ],
+    answer:
+  },
+  {
+  // question 17 
+    question: "",
+    options: [
+  
+    ],
+    answer:
+  },
+  {
+  // question 18 
+    question: "",
+    options: [
+  
+    ],
+    answer:
+  },
+  {
+  // question 19 
+    question: "",
+    options: [
+  
+    ],
+    answer:
+  },
+  {
+  // question 20 
+    question: "",
+    options: [
+  
+    ],
+    answer:
+  },
+];
